790a33c806a2942971d43282ab8d9816ef371bd7
Mikhail <mnasonov@gmail.com>
2024-12-17 16:16:13 +0400

    Add regular expression validators for different fields

--- lab_3/main.py
+++ lab_3/main.py
@@ -2,3 +2,16 @@ import re
 import csv
 import json
 from typing import List
+
+VALIDATORS = {
+    "email": re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+    "height": re.compile(r'^\d\.\d{2}$'),
+    "inn": re.compile(r'^\d{12}$'),
+    "passport": re.compile(r'^\d{2} \d{2} \d{6}$'),
+    "occupation": re.compile(r'^[а-яА-ЯёЁa-zA-Z]+(-[а-яА-ЯёЁa-zA-Z]+)*$'),
+    "latitude": re.compile(r'^-?\d+(\.\d+)?$'),
+    "hex_color": re.compile(r'^#[0-9a-fA-F]{6}$'),
+    "issn": re.compile(r'^\d{4}-\d{4}$'),
+    "uuid": re.compile(r'^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$'),
+    "time": re.compile(r'^([01]\d|2[0-3]):[0-5]\d:[0-5]\d\.\d{6}$')
+}
